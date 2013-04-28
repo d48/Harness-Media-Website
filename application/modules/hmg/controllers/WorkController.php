bb69680deb86ce7ce7e9d8fb6657835849a78072
Ryan Regalado <ryan@design48.net>
2013-04-28 00:34:27 -0700

    content for work page

--- application/modules/hmg/controllers/WorkController.php
+++ application/modules/hmg/controllers/WorkController.php
@@ -62,7 +62,13 @@ class HMG_WorkController extends Zend_Controller_Action
             'Mobile/Tablet',
             'Web UI/UX',
             'Mobile/iPad App',
-            'Customized Social Experience'
+            'Customized Social Experience',
+            'Performance - Gomez',
+            'Personalization - Baynote',
+            'Email Marketing - Lyris',
+            'Analytics - Google Analytics',
+            'Search - Endeca',
+            'Social - Wordpress, Gorilla'
         );
 
         $services = array(
@@ -85,7 +91,8 @@ class HMG_WorkController extends Zend_Controller_Action
                     $related[14]
                 ), 
             'title' => 'NIKE US OPEN',
-            'thumb' => 'hmg_tn_8020.jpg'
+            'thumb' => 'hmg_tn_usopen.jpg',
+            'tech'  => $services
         );
 
         // create duplicates and update name
@@ -94,28 +101,87 @@ class HMG_WorkController extends Zend_Controller_Action
         $oneill['description'] = 'Provided creative direction for e-commerce, digital and ongoing marketing support to reach and grow overall user base. Drove primary engagement through email marketing and social channels. As the online business grew I collaborated with teams to develop in-store retail marketing strategy and launched community marketing programs for each storefront and outlet locations.';
         $oneill['thumb'] = 'hmg_tn_oneill.jpg';
         $oneill['services'] = array(
-            $related[12],
-            $related[13],
-            $related[3],
-            $related[14]
+            $related[0],
+            $related[15],
+            $related[16],
+            $related[17],
+            $related[18]
         );
 
 
         $tlfi = $nike;
         $tlfi['title'] = 'TLFI';
+        $tlfi['description'] = 'Provided Art Direction, E-commerce UI design, ongoing marketing support to reach overall business (sales and marketing) objectives. Collaborated and created marketing/promotions calendar and drove the majority of sales via email social marketing campaigns. ';
+        $tlfi['thumb'] = 'hmg_tn_tlfi.jpg';
+        $tlfi['services'] = array(
+            $related[0],
+            $related[15],
+            $related[17],
+            $related[18]
+        );
+
 
         $killerdana = $nike;
         $killerdana['title'] = 'KILLER DANA';
+        $killerdana['description'] = 'The redesign of the Killer Dana e-commerce website and introduction the overall digital/social media strategy was primarily hands role.  The design and management of our user experience, front-end web-development, allocation of production resources, site merchandising, affiliate marketing strategy, re-marketing management and all email marketing was a huge challenge in which our team approached with eyes wide open and produced a powerful shopping experience that was no less than innovative. Key product channels were selected and managed to direct users to destinations to convert. ';
+        $killerdana['services'] = array(
+            $related[0],
+            $related[5],
+            $related[6],
+            $related[7],
+            $related[8],
+            $related[9],
+            $related[10],
+            $related[11]
+        );
+
+
 
         $contiki = $nike;
         $contiki['title'] = 'CONTIKI';
+        $contiki['description'] = 'Contiki is an exclusive travel agent/curator of 18-34 something vacations decided to launch an e-commerce extension of their brand that aligned with their 18-34 demographic. The site engaged users throughout the shopping experience by suggesting trips based on their location, search filters and item selections within the cart. Collections were also tailored for high-booking trips and cross-sold into each trip at different triggers throughout the site.';
+        $contiki['services'] = array(
+            $related[0],
+            $related[19],
+            $related[15],
+            $related[7],
+            $related[17],
+            $related[9]
+        );
+        $contiki['thumb'] = 'hmg_tn_contiki.jpg';
+
 
         $fmf = $nike;
         $fmf['title'] = 'FMF';
+        $fmf['description'] = 'Southern California is full of it. Dirt that is. Hence the existence of FMF, the original California aftermarket manufacturer for Moto and Off-road parts. Launching their new e-commerce site was a ride in itself. Learning everything about the Moto industry, parts, riding styles and more. Really good stuff. Site went off with a bang and ended with a hi-flying Best Trick show to launch the new site.';
+        $fmf['services'] = array(
+            $related[0],
+            $related[5],
+            $related[15],
+            $related[7],
+            $related[17],
+            $related[9]
+        );
+        $fmf['thumb'] = 'hmg_tn_fmf.jpg';
+
+
 
         $skateboards = $nike;
         $skateboards['title'] = 'SKATEBOARDS';
+        $skateboards['description'] = 'A première e-commerce destination for the skate industry we re-branded the entire business from the ground up. Adding our full blue chip commerce and digital package this site launched as a game changer compared to its competitors and gained traction within the 15-24 demographic.';
+        $skateboards['services'] = array(
+            $related[0],
+            $related[5],
+            $related[15],
+            $related[16],
+            $related[17],
+            $related[20],
+            $related[18]
+        );
+        $skateboards['thumb'] = 'hmg_tn_skate.jpg';
 
+
+        
         $rachelroy = $nike;
         $rachelroy['title'] = 'RACHEL ROY'; 
         $rachelroy['description'] = 'Designed and managed creative, development/UAT/QA processes and presented to C and VP level executives for concepts and direction of web positioning. Produced multiple micro-sites, web-redesign(s), various digital campaigns, sought to implement digital/e-commerce best practices throughout each brand within the organization.';
